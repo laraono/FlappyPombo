dcd7a54278fc4adb8629825ee6a8b24843e2e09e
Lara Ono <81042826+laraono@users.noreply.github.com>
2023-11-08 00:23:07 -0300

    login

--- index.php
+++ index.php
@@ -1,24 +1,34 @@
+    <?php
+        require "nav.php";
+    ?>
     <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Emojindle</title>
+        <title>FlappyPombo</title>
         <link rel="stylesheet" href="css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
-        <div class="container">
-            <h1>Jogo de Palavras</h1>
-            <p>Insira a palavra correta e pressione Enter para jogar.</p>
-            <p id="targetWord">Palavra Alvo: <span id="targetWordEmoji"></span></p>
-            <input type="text" id="userInput" class="form-control" placeholder="Insira a palavra">
-            <p id="feedback"></p>
-            <p id="score">Pontuação: 0</p>
-            <p id="time">Tempo:</p>
-            <button class="btn btn-primary" id="startButton">Iniciar Jogo</button>
+    <section class="container">
+        <div class="row justify-content-center">
+        <h1 class="col-md-auto">Olá, <?php echo $user?>! Clique aqui para iniciar uma partida:</h1>
         </div>
+        <div class="row justify-content-center">
+            <div class="col-md-auto">
+            <a class="btn btn-success" href=" <?php if ($login): ?>         
+            flappy.php
+            <?php else: ?> 
+            login.php    
+            <?php endif; ?>
+            ">Iniciar partida</a>
+            </div>
+    
+        </div>
+        
+        
+    </section>
 
-        <script src="js/main.js"></script>
     </body>
     </html>
